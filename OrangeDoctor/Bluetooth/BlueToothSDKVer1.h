aa57c7175a98842a566f044a0a9d54dbb2f10d27
wangshuo <9708022@qq.com>
2015-03-03 21:25:32 +0800

    asy

---
+++ OrangeDoctor/Bluetooth/BlueToothSDKVer1.h
@@ -0,0 +1,13 @@
+//
+//  BlueToothSDKVer1.h
+//  OrangeDoctor
+//
+//  Created by singlew on 15/3/3.
+//  Copyright (c) 2015年 Orange. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BlueToothSDKVer1 : NSObject
+
+@end
---
+++ OrangeDoctor/Bluetooth/BlueToothSDKVer1.m
@@ -0,0 +1,13 @@
+//
+//  BlueToothSDKVer1.m
+//  OrangeDoctor
+//
+//  Created by singlew on 15/3/3.
+//  Copyright (c) 2015年 Orange. All rights reserved.
+//
+
+#import "BlueToothSDKVer1.h"
+
+@implementation BlueToothSDKVer1
+
+@end
